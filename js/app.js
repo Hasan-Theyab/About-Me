006a55e57c5ec039bc45b3b2f290da14269a0beb
Hasan-Theyab <hhdoop70@gmail.com>
2021-07-05 19:22:59 +0300

    new

--- js/app.js
+++ js/app.js
@@ -1,85 +1,49 @@
 'use strict';
-let userName=prompt("What is your name?");
-alert("Hello,"+ userName + "welcome to our website");
+let username=prompt("What is your name ?");
+console.log(username)
 
-let favouriteColor=prompt('Is my favourite color blue?');
+let feeling = confirm("how are you ?")
+console.log(feeling)
 
-favouriteColor=favouriteColor.toLowerCase();
-
-if (favouriteColor===y||favouriteColor===yes) {
-    //console.log("You answered correct answer");
-    alert("Good job, That's a correct answer");
-} elseif (favouriteColor===n||favouriteColor===no)
+if(feeling === true)
 {
-    //console.log("You answered wrong answer");
-    alert("That's a wrong answer, try again");
-}else {
-    alert("Please only answer with y/n or yes/no");
+    alert("Ok good to know you're ok");
 }
- 
-let favouriteCar=prompt('Is my favourite Mercedes?');
-
-favouriteCar=favouriteCar.toLowerCase();
-
-if (favouriteCar===y||favouriteCar===yes) {
-    //console.log("You answered wrong answer");
-    alert("That's a wrong answer, try again");
-} elseif (favouriteCar===n||favouriteCar===no){
-    //console.log("You answered correct answer");
-    alert("Good job, That's a correct answer");
-} else{
-    alert("Please only answer with y/n or yes/no");
+else
+{
+    alert("Get well soon")
 }
 
+alert("Welcome " +username)
+alert("I want to play a game with you")
 
-let favouriteFruit=prompt('Is my favourite fruit banana?');
-
-favouriteFruit=favouriteFruit.toLowerCase();
-
-if (favouriteFruit===y||favouriteFruit===yes) {
-    //console.log("You answered correct answer");
-    alert("Good job, That's a correct answer");
-} elseif (favouriteFruit===n||favouriteFruit===no){
-    //console.log("You answered wrong answer");
-    alert("That's a wrong answer, try again");
-} else{
-    alert("Please only answer with y/n or yes/no");
+let major = confirm("Do you study programming ?")
+if(major === true)
+    {
+        alert("correct answer")
+    }
+    else
+    {
+        alert("incorrect answer")
+    }
+let course=prompt("What kind of programming do you study ? (Java, HTML, Python or JS)")
+if(course == "java"|| "html" || "python" || "js")
+{
+    alert("OK")
 }
- 
-let favouriteHobby=prompt('Is my favourite hobby drawing?');
-
-favouriteHobby=favouriteHobby.toLowerCase();
-
-if (favouriteHobby===y||favouriteHobby===yes) {
-    //console.log("You answered wrong answer");
-    alert("That's a wrong answer, try again");
-} elseif (favouriteHobby===n||favouriteHobby===no){
-    //console.log("You answered correct answer");
-    alert("Good job, That's a correct answer");
-} else{
-    alert("Please only answer with y/n or yes/no");
+else
+{
+    alert("invalid course")
 }
 
-let favouriteDrink=prompt('Is my favourite drink coffee?');
-
-favouriteDrink=favouriteDrink.toLowerCase();
-
-if (favouriteDrink===y||favouriteDrink===yes) {
-    //console.log("You answered wrong answer");
-    alert("That's a wrong answer, try again");
-} elseif (favouriteDrink===n||favouriteDrink===no){
-    //console.log("You answered correct answer");
-    alert("Good job, That's a correct answer");
-} else{
-    alert("Please only answer with y/n or yes/no");
+let car=confirm("Do cars fly?")
+if(car === true)
+{
+    alert("incorrect")
+}
+else
+{
+    alert("correct")
 }
 
-alert("Welcome back" + userName + " the guessing game is over");
-
-
-
-
-
-
-
-
+alert("Thank you for playing " +username)
