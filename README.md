b4b0cf2d8b89b53381df3a09e88b034c21ead184
Hasan-Theyab <hhdoop70@gmail.com>
2021-07-05 18:10:24 +0300

    finished lab02

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# About-Me
+# LAB - 02
+
+## About Me Guessing Game
+
+I build this app to let the users and visitors to my webpage know more about me through a guessing game.It Prompts the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses. 
+
+### Author: Hasan Theyab
+
+### Links and Resources
+
+    - https://canvas.instructure.com/courses/2984298/assignments/23122309?module_item_id=49067349
+
+    - https://codefellows.github.io/code-201-guide/curriculum/class-02/project_setup
+
+    - https://codefellows.github.io/code-201-guide/curriculum/class-02/README-template.html
+
+    - https://github.com/LTUC/amman-201d33/tree/main/class-02
+
+    - https://www.markdownguide.org/basic-syntax/
+
+### Reflections and Comments
+
+        - It went very good except some little things.
+          
+          I have an observation about eslint for js file, my code didn't match completly with eslint.
+          
+          It took me 4 hours to complete this assignment,before I startedthis assignment I thought it would take me 1-2 hours to complete this assignment
+
+          I see myself deeply involved in planning and evaluating my learning
+
+          I will record and/or measure the knowledge that I want to acquire
+
+          I will evaluate what I want to learn continuously
+
+        - GitHub,Canvas,Windows Terminal (Ubuntu),VS Code.
---
+++ js/app.js
@@ -0,0 +1,85 @@
+'use strict';
+let userName=prompt("What is your name?");
+alert("Hello,"+ userName + "welcome to our website");
+
+let favouriteColor=prompt('Is my favourite color blue?');
+
+favouriteColor=favouriteColor.toLowerCase();
+
+if (favouriteColor===y||favouriteColor===yes) {
+    //console.log("You answered correct answer");
+    alert("Good job, That's a correct answer");
+} elseif (favouriteColor===n||favouriteColor===no)
+{
+    //console.log("You answered wrong answer");
+    alert("That's a wrong answer, try again");
+}else {
+    alert("Please only answer with y/n or yes/no");
+}
+ 
+let favouriteCar=prompt('Is my favourite Mercedes?');
+
+favouriteCar=favouriteCar.toLowerCase();
+
+if (favouriteCar===y||favouriteCar===yes) {
+    //console.log("You answered wrong answer");
+    alert("That's a wrong answer, try again");
+} elseif (favouriteCar===n||favouriteCar===no){
+    //console.log("You answered correct answer");
+    alert("Good job, That's a correct answer");
+} else{
+    alert("Please only answer with y/n or yes/no");
+}
+
+
+let favouriteFruit=prompt('Is my favourite fruit banana?');
+
+favouriteFruit=favouriteFruit.toLowerCase();
+
+if (favouriteFruit===y||favouriteFruit===yes) {
+    //console.log("You answered correct answer");
+    alert("Good job, That's a correct answer");
+} elseif (favouriteFruit===n||favouriteFruit===no){
+    //console.log("You answered wrong answer");
+    alert("That's a wrong answer, try again");
+} else{
+    alert("Please only answer with y/n or yes/no");
+}
+ 
+let favouriteHobby=prompt('Is my favourite hobby drawing?');
+
+favouriteHobby=favouriteHobby.toLowerCase();
+
+if (favouriteHobby===y||favouriteHobby===yes) {
+    //console.log("You answered wrong answer");
+    alert("That's a wrong answer, try again");
+} elseif (favouriteHobby===n||favouriteHobby===no){
+    //console.log("You answered correct answer");
+    alert("Good job, That's a correct answer");
+} else{
+    alert("Please only answer with y/n or yes/no");
+}
+
+let favouriteDrink=prompt('Is my favourite drink coffee?');
+
+favouriteDrink=favouriteDrink.toLowerCase();
+
+if (favouriteDrink===y||favouriteDrink===yes) {
+    //console.log("You answered wrong answer");
+    alert("That's a wrong answer, try again");
+} elseif (favouriteDrink===n||favouriteDrink===no){
+    //console.log("You answered correct answer");
+    alert("Good job, That's a correct answer");
+} else{
+    alert("Please only answer with y/n or yes/no");
+}
+
+alert("Welcome back" + userName + " the guessing game is over");
+
+
+
+
+
+
+
+
